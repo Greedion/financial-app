2f53c0ab6e0148f1e2a8337febca25e81c4daa3e
greedion <adrian43graf@gmail.com>
2022-12-29 18:30:51 +0100

    Inital commit

---
+++ financial-app/src/main/java/com/banking/FinancialAppApplication.java
@@ -0,0 +1,13 @@
+package com.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialAppApplication.class, args);
+	}
+
+}
